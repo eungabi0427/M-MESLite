e793b1547202efbe343209119c3e9d9abb1d96c8
eungabi0427 <jhpark@samco.kr>
2022-06-20 10:38:50 +0900

    박재흥 BASE_URI 추가

--- lib/helper/base_url.dart
+++ lib/helper/base_url.dart
@@ -1,4 +1,5 @@
 class Constants {
   Constants._();
   static const BASE_URL = 'http://192.168.101.241:44300/WebService.asmx';
+  //static const BASE_URL = 'http://192.168.101.104:44300/WebService.asmx';
 }
